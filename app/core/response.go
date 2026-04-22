f423d1df0da277c12f800404a4df911d1b95015e
Irwan Rusda <irwanrusda@gmail.com>
2026-04-22 13:15:51 +0700

    fix(deployment): prevent aggressive API caching by Easypanel/Cloudflare proxies by setting strict HTTP cache-control headers

--- app/core/response.go
+++ app/core/response.go
@@ -10,6 +10,9 @@ type H map[string]interface{}
 // JSONResponse sends a standard JSON response and automatically adds success: true if code < 300
 func JSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
 	w.WriteHeader(statusCode)
 
 	// Inject success key if it's a map and code is success
